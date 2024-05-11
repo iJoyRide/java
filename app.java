bd655380e949392e72fcd6bf281b35d353f0771d
iJoyRide <sumfeis3@gmail.com>
2024-05-12 00:51:50 +0800

    time and date

--- app.java
+++ app.java
@@ -1,5 +1,17 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        int myage = 30;
+        long viewsCount = 3_123_456_789L;
+        double number = 88.99;
+        float floatNumber = 5/2;
+        char letter = 'A';
+
+        System.out.println(myage);
+        System.out.println(viewsCount);
+        System.out.println(number);
+        System.out.println(floatNumber);
+        System.out.println(letter);
+      
     }
 }
---
+++ time_date.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class time_date {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println(now);
+    }
+}
